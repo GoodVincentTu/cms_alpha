15cbddcf5955f486906a6840f7fa56a82f756c04
metatrontu@gmail.com <metatrontu@gmail.com>
2016-06-22 09:58:46 +0800

    finish basic pages skeleton

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,19 @@
 Rails.application.routes.draw do
+  resources :pages, only: [:show]
+
   namespace :admin do
     resources :pages  # /admin/pages
+  end
+
+  # select pages with slug
+  Page.where.not("slug", nil).all.each do |page|
+    get "/#{page.slug}", controller: "pages", action: "show", id: page.id
   end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+ root 'admin/pages#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
