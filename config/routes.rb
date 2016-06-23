d46413dedfcdef418520846c5b08b93bbeeda3fe
metatrontu@gmail.com <metatrontu@gmail.com>
2016-06-23 11:53:39 +0800

    Add the menu feature

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :pages, only: [:show]
 
   namespace :admin do
+    resources :menus, except: [:show]
     resources :pages  # /admin/pages
     resources :categories
   end
